12efd1f1974b113fb6f1f41e89829832d1d483bf
Jenkins <avn.hudson@garmin.com>
2025-05-05 14:53:23 -0500

    Garmin FIT SDK 21.171.0
    Change-Id: I22458474e0dad68bbdd3c6a6def3c326b6893b7a

--- tests/data.py
+++ tests/data.py
@@ -1,7 +1,7 @@
 '''data.py: Contains various byte arrays which are reused in testing the FIT SDK.'''
 
 ###########################################################################################
-# Copyright 2024 Garmin International, Inc.
+# Copyright 2025 Garmin International, Inc.
 # Licensed under the Flexible and Interoperable Data Transfer (FIT) Protocol License; you
 # may not use this file except in compliance with the Flexible and Interoperable Data
 # Transfer (FIT) Protocol License.
