bec4dd8e47f52e7d1c2737728b816160aad69e7b
Elijah Flinders <14484665+Lijah99@users.noreply.github.com>
2025-05-05 14:25:31 -0600

    Garmin FIT SDK 21.171.0
    Garmin FIT SDK 21.171.0

--- tests/data_expand_hr_mesgs.py
+++ tests/data_expand_hr_mesgs.py
@@ -1,7 +1,7 @@
 '''data_expand_hr_mesgs.py: Contains the data for running the hr_mesgs_utils tests'''
 
 ###########################################################################################
-# Copyright 2024 Garmin International, Inc.
+# Copyright 2025 Garmin International, Inc.
 # Licensed under the Flexible and Interoperable Data Transfer (FIT) Protocol License; you
 # may not use this file except in compliance with the Flexible and Interoperable Data
 # Transfer (FIT) Protocol License.
--- tests/test_hr_mesg_utils.py
+++ tests/test_hr_mesg_utils.py
@@ -1,7 +1,7 @@
 '''test_hr_mesg_utils.py: Contains the tests for the heart rate message merging functionality'''
 
 ###########################################################################################
-# Copyright 2024 Garmin International, Inc.
+# Copyright 2025 Garmin International, Inc.
 # Licensed under the Flexible and Interoperable Data Transfer (FIT) Protocol License; you
 # may not use this file except in compliance with the Flexible and Interoperable Data
 # Transfer (FIT) Protocol License.
